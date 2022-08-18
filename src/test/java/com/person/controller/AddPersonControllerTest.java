85038acb9aa40b3a4668714f106447fa8f302150
shali <shali@ShaliniD>
2022-08-17 22:35:22 -0500

    Added active profile "test" on test classes.

--- src/test/java/com/person/controller/AddPersonControllerTest.java
+++ src/test/java/com/person/controller/AddPersonControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.person.repositories.PersonRepository;
@@ -16,6 +17,7 @@ import com.person.services.PersonAddService;
 
 @AutoConfigureMockMvc
 @SpringBootTest
+@ActiveProfiles("test")
 public class AddPersonControllerTest {
 
 	@Autowired
